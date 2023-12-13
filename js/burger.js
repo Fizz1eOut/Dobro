7f46e0d50d71a2c3b15899a9bf15f05dece18d5f
FizzleOut <fizzleout1337@gmail.com>
2023-12-13 19:30:35 +0100

    New project project Dobro, created header and burger

---
+++ js/burger.js
@@ -0,0 +1,34 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.nav');
+const overlay = document.querySelector('.overlay');
+
+const lockScroll = () => {
+  document.body.classList.add('lock');
+};
+const unlockScroll = () => {
+  document.body.classList.remove('lock');
+};
+const scrollTop = () => {
+  menu.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
+
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('burger')) {
+    burger.classList.toggle('burger--active');
+    menu.classList.toggle('open');
+    overlay.classList.toggle('open');
+    lockScroll();
+  } else {
+    unlockScroll();
+  }
+});
+
+overlay.addEventListener('click', () => {
+  burger.classList.remove('burger--active');
+  menu.classList.remove('open');
+  overlay.classList.remove('open');
+  unlockScroll();
+});
